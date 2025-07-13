790f7a6f82efadeabfc102722eac16514cbfb617
Abraham Raphael <abrahamraphael1995@gmail.com>
2025-07-13 21:50:47 +0100

    Add script for 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const numberOfArguments = process.argv.length - 2;
+if (numberOfArguments === 0) {
+    console.log("No argument");
+} else if (numberOfArguments === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
